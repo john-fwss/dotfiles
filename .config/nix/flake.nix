d84c98e96b9b82fd3bf7c7e51ca0f1ff0a11e17a
john <john@fwss.us>
2024-10-22 20:36:09 -0400

    waybar: remove unused temperature module

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -3,9 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
 
-  outputs = { self, nixpkgs, ...}@inputs: {
+  outputs = { self, nixpkgs, nixos-wsl, ...}@inputs: {
     nixosConfigurations.onyx = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -28,6 +29,27 @@
 	./xdg.nix
       ];
     };
+    nixosConfigurations.gengar = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+	./environment.nix
+        ./fonts.nix
+        ./i18n.nix
+        ./nix.nix
+        ./nixpkgs.nix
+        ./networking-gengar.nix
+        ./programs.nix
+        ./system.nix
+        ./time.nix
+        ./users.nix
+        ./virtualisation.nix
+        nixos-wsl.nixosModules.default
+	{
+          system.stateVersion = "24.05";
+          wsl.enable = true;
+	  wsl.defaultUser = "jjb";
+        }
+      ];
+    };
   };
 }
-
---
+++ .config/nix/networking-gengar.nix
@@ -0,0 +1,10 @@
+{ ... }:
+
+{
+  networking = {
+    hostName = "gengar";
+    networkmanager = {
+      enable = true;
+    };
+  };
+}
